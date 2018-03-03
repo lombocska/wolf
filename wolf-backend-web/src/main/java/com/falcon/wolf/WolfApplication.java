c54d1ad8f9f57c31f57ff280fa3f5c1ab7fffcc0
lombos.monika <monika.lombos@eradius.hu>
2018-03-03 14:34:19 +0100

    WOLF-5 Cleaned pom and deleted empty automtically created appliationtests

--- wolf-backend-web/src/main/java/com/falcon/wolf/WolfApplication.java
+++ wolf-backend-web/src/main/java/com/falcon/wolf/WolfApplication.java
@@ -2,10 +2,10 @@ package com.falcon.wolf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class WolfApplication {
 
     public static void main(String[] args) {
