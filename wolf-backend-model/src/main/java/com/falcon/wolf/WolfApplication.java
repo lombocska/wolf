014e577965660584a4178fe2f4b52313f944df4c
lombos.monika <monika.lombos@eradius.hu>
2018-03-03 09:11:25 +0100

    WOLF-5 Create wolf-backend-model modul

---
+++ wolf-backend-model/src/main/java/com/falcon/wolf/WolfApplication.java
@@ -0,0 +1,12 @@
+package com.falcon.wolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfApplication.class, args);
+	}
+}
