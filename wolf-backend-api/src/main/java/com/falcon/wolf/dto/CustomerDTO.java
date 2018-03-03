56661269cd6ac10d92adac4304964f3835bd4ae6
lombos.monika <monika.lombos@eradius.hu>
2018-03-03 19:27:24 +0100

    WOLF-6 Tested resourceImpl

--- wolf-backend-api/src/main/java/com/falcon/wolf/dto/CustomerDTO.java
+++ wolf-backend-api/src/main/java/com/falcon/wolf/dto/CustomerDTO.java
@@ -1,10 +1,16 @@
 package com.falcon.wolf.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CustomerDTO {
 
     private Long id;
