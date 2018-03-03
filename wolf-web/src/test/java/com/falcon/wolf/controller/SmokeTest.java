807a740460f0853808c0575614e733a3396e2970
lombos.monika <monika.lombos@eradius.hu>
2018-03-03 20:04:33 +0100

    WOLF-6 Check the context is creating CustomerController

---
+++ wolf-web/src/test/java/com/falcon/wolf/controller/SmokeTest.java
@@ -0,0 +1,22 @@
+package com.falcon.wolf.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private CustomerController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
