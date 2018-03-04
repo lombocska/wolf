b6870a0a8ae9ebf625aa2ce420a593f357a843d4
lombos.monika <monika.lombos@eradius.hu>
2018-03-04 01:53:35 +0100

    WOLF-10 JSON parse working flawlessly & table get value in real time bywebsocket (SocketJS)

--- wolf-web/src/main/resources/static/js/app.js
+++ wolf-web/src/main/resources/static/js/app.js
@@ -6,11 +6,16 @@ function connect() {
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/home', function (customerDTO) {
-            showNewCustomer(JSON.parse(customerDTO.body).content);
+            showNewCustomer(JSON.parse(customerDTO.body));
         });
     });
 }
 
 function showNewCustomer(customerDTO) {
-    $("#customer").append("<tr><td>" + customerDTO.name + "</td></tr>");
+    $("#customer").append(
+        "<tr>" +
+        "<td>" + customerDTO.name + "</td>" +
+        "<td>" + customerDTO.address + "</td>" +
+        "<td>" + customerDTO.email + "</td>" +
+        "</tr>");
 }
