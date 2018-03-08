76ade053288d2dffbbe7718a6cbe81c6c123d792
lombos.monika <monika.lombos@eradius.hu>
2018-03-08 18:29:54 +0100

    WOLF-14 Saving controller get into saving package to hide /home endpoint from swagger

--- wolf-web/src/main/java/com/falcon/wolf/controller/saving/CustomerSavingController.java
+++ wolf-web/src/main/java/com/falcon/wolf/controller/saving/CustomerSavingController.java
@@ -1,11 +1,10 @@
-package com.falcon.wolf.controller;
+package com.falcon.wolf.controller.saving;
 
+import com.falcon.wolf.controller.CustomerResponse;
 import com.falcon.wolf.dto.CustomerDTO;
 import com.falcon.wolf.resource.CustomerResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +16,7 @@ import javax.validation.Valid;
 /**
  * CustomerSavingController is a simple REST Controller.
  * It provides /save-customer endpoint to save customer by the RequestBody.
- *
+ * <p>
  * If /save-customer endpoint  is called SimpMessageTemplate will call the /topic/home endpoint to
  * send newly saved customer data to /home endpoint.
  */
@@ -25,19 +24,19 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 @RequestMapping
-public class CusomerSavingController {
+public class CustomerSavingController {
 
     private final CustomerResource customerResource;
     private final SimpMessagingTemplate template;
 
     @Autowired
-    public CusomerSavingController(CustomerResource customerResource, SimpMessagingTemplate template) {
+    public CustomerSavingController(CustomerResource customerResource, SimpMessagingTemplate template) {
         this.customerResource = customerResource;
         this.template = template;
     }
 
     @PostMapping(value = "/save-customer")
-    public CustomerResponse saveCustomers(@Valid @RequestBody CustomerDTO customerDTO) {
+    public CustomerResponse saveCustomer(@Valid @RequestBody CustomerDTO customerDTO) {
         log.info("Received customer: {}", customerDTO.getName());
         customerDTO = customerResource.saveCustomer(customerDTO);
         template.convertAndSend("/topic/home", customerDTO);
@@ -45,6 +44,5 @@ public class CusomerSavingController {
                 .customerDTOs(customerDTO)
                 .statusCode(200)
                 .build();
-//        return new ResponseEntity<>(customerDTO, HttpStatus.OK);
     }
 }
