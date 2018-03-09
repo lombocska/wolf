bd0bba036752f3ab31f63b40d047debe69060f64
lombos.monika <monika.lombos@eradius.hu>
2018-03-09 23:20:02 +0100

    WOLF-21 Added exception handling for saving

--- wolf-backend-service/src/main/java/com/falcon/wolf/listener/CustomerMessageListener.java
+++ wolf-backend-service/src/main/java/com/falcon/wolf/listener/CustomerMessageListener.java
@@ -2,10 +2,12 @@ package com.falcon.wolf.listener;
 
 import com.falcon.wolf.entity.Customer;
 import com.falcon.wolf.implementation.CustomerDTO;
+import com.falcon.wolf.implementation.EntityConstraintViolationException;
 import com.falcon.wolf.repository.CustomerRepository;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -24,7 +26,12 @@ public class CustomerMessageListener {
     public void receiveMessage(Map<String, String> message) {
         log.info("Received customerMessageMap.");
         CustomerDTO customerDTO = new Gson().fromJson(message.get("customerName"), CustomerDTO.class);
-        customerRepository.save(fromCustomerDTO(customerDTO));
+        try {
+            Customer customer = customerRepository.save(fromCustomerDTO(customerDTO));
+        } catch (DataIntegrityViolationException ex) {
+            throw new EntityConstraintViolationException(Customer.class.getSimpleName(), customerDTO.getName());
+        }
+
         log.info("Message processed.");
     }
 
