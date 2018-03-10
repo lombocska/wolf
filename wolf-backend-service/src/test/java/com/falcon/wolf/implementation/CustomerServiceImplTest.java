711634f441277120eb816e7b0075346936ad42f5
lombos.monika <monika.lombos@eradius.hu>
2018-03-10 16:39:14 +0100

    WOLF-21 Test fails

--- wolf-backend-service/src/test/java/com/falcon/wolf/implementation/CustomerServiceImplTest.java
+++ wolf-backend-service/src/test/java/com/falcon/wolf/implementation/CustomerServiceImplTest.java
@@ -9,8 +9,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,16 +23,20 @@ public class CustomerServiceImplTest {
     public final ExpectedException thrown = ExpectedException.none();
     @Mock
     private CustomerRepository customerRepository;
+    @Mock
+    private RabbitTemplate rabbitTemplate;
     @InjectMocks
     private CustomerServiceImpl customerServiceImpl;
 
     @Test
-    public void testSaveCustomer() {
+    public void testSendCustomerMessage() {
         customerServiceImpl.sendCustomerMessage(CustomerDTO
                 .builder()
                 .address("SomeAddress")
+                .name("SomeName")
+                .email("some@email.com")
                 .build());
-        verify(customerRepository).save(any(Customer.class));
+        verify(rabbitTemplate).convertAndSend(anyString(), anyMap());
     }
 
     @Test
