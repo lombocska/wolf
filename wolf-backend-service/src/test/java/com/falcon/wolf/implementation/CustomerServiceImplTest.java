dd55765492af9a9f04796339bbab954209265bbc
lombos.monika <monika.lombos@eradius.hu>
2018-03-10 20:54:27 +0100

    WOLF-21 Fixed test

--- wolf-backend-service/src/test/java/com/falcon/wolf/implementation/CustomerServiceImplTest.java
+++ wolf-backend-service/src/test/java/com/falcon/wolf/implementation/CustomerServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.falcon.wolf.implementation;
 
-import com.falcon.wolf.entity.Customer;
 import com.falcon.wolf.repository.CustomerRepository;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,13 +10,19 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import java.util.HashMap;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CustomerServiceImplTest {
+
+    private static final String ADDRESS = "address";
+    private static final String NAME = "name";
+    private static final String EMAIL = "some@email.com";
 
     @Rule
     public final ExpectedException thrown = ExpectedException.none();
@@ -27,17 +32,6 @@ public class CustomerServiceImplTest {
     private RabbitTemplate rabbitTemplate;
     @InjectMocks
     private CustomerServiceImpl customerServiceImpl;
-
-    @Test
-    public void testSendCustomerMessage() {
-        customerServiceImpl.sendCustomerMessage(CustomerDTO
-                .builder()
-                .address("SomeAddress")
-                .name("SomeName")
-                .email("some@email.com")
-                .build());
-        verify(rabbitTemplate).convertAndSend(anyString(), anyMap());
-    }
 
     @Test
     public void testFindAll() {
